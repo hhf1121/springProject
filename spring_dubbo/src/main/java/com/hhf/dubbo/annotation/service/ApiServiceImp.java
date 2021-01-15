848faf05cefc13a757512d5a6ee7dac5a5e66d77
hehf <hehf@yimidida.com>
2021-01-15 11:23:28 +0800

    优化dubbo调用

--- spring_dubbo/src/main/java/com/hhf/dubbo/annotation/service/ApiServiceImp.java
+++ spring_dubbo/src/main/java/com/hhf/dubbo/annotation/service/ApiServiceImp.java
@@ -2,8 +2,10 @@ package com.hhf.dubbo.annotation.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.hhf.dubbo.annotation.service.ApiService;
+import lombok.extern.slf4j.Slf4j;
 
 
+@Slf4j
 @Service(version = "0.0.1",interfaceClass = ApiService.class,interfaceName = "com.hhf.dubbo.annotation.service.ApiService")
 @org.springframework.stereotype.Service
 public class ApiServiceImp implements ApiService {
@@ -11,6 +13,7 @@ public class ApiServiceImp implements ApiService {
 
     @Override
     public String getDataByInfo(String info) {
+        log.info("调用成功...");
         return "is me!=====>"+info;
     }
 }
